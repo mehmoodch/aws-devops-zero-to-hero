adff29b23ac19544c85dd5e0c590f2eb0c8b661a
Mehmood Asghar <45485901+mehmoodch@users.noreply.github.com>
2024-06-06 16:57:24 +0400

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, Mehmood asghar!'
 
 if __name__ == '__main__':
     app.run()
